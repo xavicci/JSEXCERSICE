6b825ef8980bd71fdbf07851d49fc37a54405001
Xavi Flores <xa_franc@live.com>
2023-09-21 14:13:19 -0600

    migrar

--- 09-DOM/09-DOM.js
+++ 09-DOM/09-DOM.js
@@ -1,33 +1,38 @@
 // diferencia entre queryselector y getElementById
 
+// const h1 = document.querySelector('h1');
+// const classparrafo = document.querySelector('.class-parrafo');
+// const idparrafo = document.querySelector('#id-parrafo');
+// const input = document.querySelector('input');
+
+// console.log({
+//     h1,
+//     classparrafo,
+//     idparrafo,
+//     input,
+// })
+
+// h1.innerHTML = "Cambiando mi titulo h1";
+
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class','cambio-de-class');
+// console.log(h1.getAttribute('class'));
+
+// h1.classList.add('rojo');
+// h1.classList.add('verde');
+// h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
+
+
+// input.value=4656;
+
+// const tagh2 = document.createElement('h2');
+// tagh2.innerText="woww"
+
+// classparrafo.append(tagh2);
+
 const h1 = document.querySelector('h1');
-const classparrafo = document.querySelector('.class-parrafo');
-const idparrafo = document.querySelector('#id-parrafo');
-const input = document.querySelector('input');
-
-console.log({
-    h1,
-    classparrafo,
-    idparrafo,
-    input,
-})
-
-h1.innerHTML = "Cambiando mi titulo h1";
-
-console.log(h1.getAttribute('class'));
-h1.setAttribute('class','cambio-de-class');
-console.log(h1.getAttribute('class'));
-
-h1.classList.add('rojo');
-h1.classList.add('verde');
-h1.classList.remove('verde');
-h1.classList.toggle('verde');
-h1.classList.contains('verde');
-
-
-input.value=4656;
-
-const tagh2 = document.createElement('h2');
-tagh2.innerText="woww"
-
-classparrafo.append(tagh2);
+const input1 = document.querySelector('.class-parrafo');
+const input2 = document.querySelector('#id-parrafo');
+const btn = document.querySelector('input');
