aa3e2ace971c68d43c67eefbe4f18933996c1730
Xavi Flores <xa_franc@live.com>
2023-09-22 13:50:43 -0600

    incorporacion otro curso

---
+++ JS/ECMASCRIPT/src/es6/00-let-const.js
@@ -0,0 +1,13 @@
+var nombre = 'Oscar';
+nombre = 'Xavier';
+
+console.log(nombre);
+
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'Cat';
+console.log(animal);
