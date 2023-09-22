33df436673729fb32afb4026eb2d04ad3b4c1fb8
Xavi Flores <xa_franc@live.com>
2023-09-22 08:15:53 -0600

    manipulacionDOM

--- 09-DOM/09-DOM.js
+++ 09-DOM/09-DOM.js
@@ -33,6 +33,22 @@
 // classparrafo.append(tagh2);
 
 const h1 = document.querySelector('h1');
-const input1 = document.querySelector('.class-parrafo');
-const input2 = document.querySelector('#id-parrafo');
-const btn = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#resultado');
+const form = document.querySelector('#form');
+
+btn.addEventListener('click', btnOnClick); //con este evento ya no hace falta mandarlo a llamar desde el HTML onClick
+
+form.addEventListener('submit', sumForm); //con este evento ya no hace falta mandarlo a llamar desde el HTML onClick
+
+function btnOnClick() { // toma el valor de los dos input y devuelve su concatenacion
+    pResult.innerHTML = input1.value * 1 + input2.value * 1;
+};
+
+function sumForm(event) { // toma el valor de los dos input y devuelve su concatenacion
+    pResult.innerHTML = input1.value * 1 + input2.value * 1;
+    event.preventDefault();
+};
+
