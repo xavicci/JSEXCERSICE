6c74134fe4948f22332ff7529ea07b7bcad34828
Xavi Flores <xa_franc@live.com>
2023-11-15 14:20:53 -0600

    final

--- JS/POO/poo4.js
+++ JS/POO/poo4.js
@@ -44,6 +44,88 @@ function requiredParam(param) {
     throw new Error(param + " es obligatorio");
 }
 
+
+class SuperObject {
+    static isObject(subject) {
+        return typeof subject == "object";
+    }
+
+
+    // static deepCopy(subject) {
+    //     let copySubject;
+    //     const subjectIsObject = isObject(subject);
+    //     const subjectIsArray = isArray(subject);
+
+    //     if (subjectIsArray) {
+    //         copySubject = [];
+    //     } else if (subjectIsObject) {
+    //         copySubject = {};
+    //     } else {
+    //         return subject;
+    //     }
+
+    //     for (key in subject) {
+    //         const keyIsObject = isObject(subject[key]);
+
+    //         if (keyIsObject) {
+    //             copySubject[key] = this.deepCopy(subject[key]);
+    //         } else {
+    //             if (subjectIsArray) {
+    //                 copySubject.push(subject[key]);
+    //             } else {
+    //                 copySubject[key] = subject[key];
+    //             }
+    //         }
+
+    //     }
+
+    //     return copySubject;
+
+    // }
+}
+
+function SuperObjectProto() {
+
+    SuperObject.isObject = function (subject) {
+        return typeof subject == "object";
+    }
+
+    SuperObject.deepCopy = function (subject) {
+        let copySubject;
+        const subjectIsObject = isObject(subject);
+        const subjectIsArray = isArray(subject);
+
+        if (subjectIsArray) {
+            copySubject = [];
+        } else if (subjectIsObject) {
+            copySubject = {};
+        } else {
+            return subject;
+        }
+
+        for (key in subject) {
+            const keyIsObject = isObject(subject[key]);
+
+            if (keyIsObject) {
+                copySubject[key] = this.deepCopy(subject[key]);
+            } else {
+                if (subjectIsArray) {
+                    copySubject.push(subject[key]);
+                } else {
+                    copySubject[key] = subject[key];
+                }
+            }
+
+        }
+
+        return copySubject;
+
+    }
+
+
+
+}
+
 function createLearningPath({
     name = requiredParam("name"), // Campo es obligatorio
     courses = [], // Lista de Cursos que pertencen a la ruta de aprendizaje
